54e295491dc7070a658ec054c06386349377179b
henry-herrmann <henryherrmann.tv@gmail.com>
2022-06-08 19:04:22 +0200

    first commit

--- ddns/server.js
+++ ddns/server.js
@@ -3,6 +3,7 @@ const fs = require("fs").promises;
 
 const run = () =>{
     dnsd.createServer(async (req, res) =>{
+        console.log(req)
         if(req.question[0].name == "home.herrmann.page"){
             const content = await fs.readFile("../ip.txt").catch(err =>{
                 return res.end("202.61.201.124");
@@ -13,6 +14,7 @@ const run = () =>{
             return res.end("202.61.201.124");
         }
     }).listen(53, "202.61.201.124");
+    console.log("[INFO] Nameserver successfully started.")
 }
 
 module.exports.run = run;
